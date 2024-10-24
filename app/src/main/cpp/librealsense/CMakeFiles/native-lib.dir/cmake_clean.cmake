bf565a17199c445330d6c5f091ce884a43d98e86
Sirui Li <sli869@wisc.edu>
2024-10-23 23:18:52 -0500

    First commit

---
+++ app/src/main/cpp/librealsense/CMakeFiles/native-lib.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/native-lib.dir/native-lib.cpp.o"
+  "CMakeFiles/native-lib.dir/native-lib.cpp.o.d"
+  "build/libnative-lib.pdb"
+  "build/libnative-lib.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/native-lib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
